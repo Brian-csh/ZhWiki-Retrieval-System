853c91d5f7864fcc35f3759436e8f1c1ecbcbeae
h-guo18 <h-guo18@foxmail.com>
2022-10-12 07:55:40 +0000

    optimize for embedding searching

--- knn.py
+++ knn.py
@@ -9,20 +9,20 @@ from tqdm import tqdm
 from transformers import AutoTokenizer
 from src.contriever import Contriever
 import argparse
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 TOKENIZER = AutoTokenizer.from_pretrained('mcontriever')
 print("tokenizer loaded")
-MODEL = Contriever.from_pretrained('mcontriever')
+MODEL = Contriever.from_pretrained('mcontriever').to(device)
 print("model loaded")
 
-device = device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
 
 
 # computes L2 distance of two tensors
-def l2_distance(a, b):
-    dist = torch.square(a - b)
-    dist = torch.sum(dist)
-    return math.sqrt(dist)
+# def l2_distance(a, b):
+#     dist = torch.square(a - b)
+#     dist = torch.sum(dist)
+#     return math.sqrt(dist)
 
 
 # retrieves embeddings from pt file
@@ -32,20 +32,20 @@ def get_batch_embeddings(file_index):
 
 
 # update and maintain the knn list
-def update_nn(nn_list, candidate, k):
-    if len(nn_list) < k:
-        nn_list.append(candidate)
-        return
-    for nn in nn_list:
-        if candidate["dist"] < nn["dist"]:
-            nn_list.remove(nn)
-            nn_list.append(candidate)
-            return
+# def update_nn(nn_list, candidate, k):
+#     if len(nn_list) < k:
+#         nn_list.append(candidate)
+#         return
+#     for nn in nn_list:
+#         if candidate["dist"] < nn["dist"]:
+#             nn_list.remove(nn)
+#             nn_list.append(candidate)
+#             return
 
 
 # retrieve text data given the nn_list which contains the index of the chunks
-def retrieve_texts(nn_list):
-    line_numbers = [nn["index"] for nn in sorted(nn_list, key=lambda d:d["dist"])]
+def retrieve_texts(nn_dict):
+    line_numbers = [nn[0] for nn in sorted(nn_dict.items(), key=lambda x:x[1])]
     results = []
     for line in line_numbers:
         results.append(json.loads(linecache.getline("database/text_database.json", line + 1)))
@@ -54,19 +54,26 @@ def retrieve_texts(nn_list):
 
 # returns k nearest neighbors of sentence
 def knn(sentence, k):
-    input = TOKENIZER(sentence, padding=True, truncation=True, return_tensors='pt')
+    input = TOKENIZER(sentence, padding=True, truncation=True, return_tensors='pt').to(device)
     query = MODEL(**input)
     query = query.to(device)
-    nn_list = []
+    nn_dict = {}
     for i in tqdm(range(39895)):
-        embeddings = get_batch_embeddings(i)
-        for idx, embedding in enumerate(embeddings):
-            embedding = embedding.to(device)
-            curr = {}
-            curr["index"] = i * 500 + idx # to change
-            curr["dist"] = l2_distance(query, embedding)
-            update_nn(nn_list, curr, k)
-    neighbors = retrieve_texts(nn_list)
+        embeddings = get_batch_embeddings(i) #list of [1,768] vectors
+        # for idx, embedding in enumerate(embeddings):
+        #     embedding = embedding.to(device)
+        #     curr = {}
+        #     curr["index"] = i * 500 + idx # to change
+        #     curr["dist"] = l2_distance(query, embedding)
+        #     update_nn(nn_list, curr, k)
+        embeddings = torch.row_stack(embeddings).detach().to(device) # [n,768] matrix, where n is the number of chunks
+        distances = torch.linalg.vector_norm(embeddings-query,ord=2,dim=1) # 2-norm, i.e. L2 distance of query to each embedding, return in shape [n]
+        sorted_dist, indices = torch.sort(distances)
+        indices += i * 500
+        nn_dict.update(dict(zip(indices[:k].tolist(),sorted_dist[:k].tolist())))
+
+        
+    neighbors = retrieve_texts(nn_dict)
     print(json.dumps(neighbors, ensure_ascii=False))
     return neighbors
 
