c23afa915d745b4237aa58a48f2a97f711ba93d6
Brian Lin <brianslin01@gmail.om>
2022-10-30 13:04:47 +0000

    Revise and optimize embed

--- embed.py
+++ embed.py
@@ -1,35 +1,54 @@
+import os
 import sys
-sys.path.insert(0, "/data/disk2/private/caishihuai/low_data_generation/retrieval_database/contriever")
+sys.path.insert(0, os.getcwd() + "/../contriever")
 import json
 import torch
 from transformers import AutoTokenizer
 from src.contriever import Contriever
+from tqdm import tqdm
+import argparse
 
-TOKENIZER = AutoTokenizer.from_pretrained('facebook/mcontriever')
-MODEL = Contriever.from_pretrained('facebook/mcontriever')
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-index = 0
-ctr = 0
-root = "results/keys/key_"
-suffix = ".pt"
-embeddings = []
+@torch.no_grad()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", help="text file containing the neighbor-continuation pairs")
+    parser.add_argument("output_dir", help="path to the output directory")
+    parser.add_argument("tensors_per_file", type=int, help="number of tensors to be stored within a single .pt file")
+    args = parser.parse_args()
 
-with open("results/text_database.json", "r") as input_file:
-    for line in input_file:
-        data = json.loads(line)
-        neighbor = data["neighbor"]
-        input = TOKENIZER(neighbor, padding=True, truncation=True, return_tensors='pt')
-        output = MODEL(**input)
-        embeddings.append(output)
-        ctr += 1
-        if ctr == 500:
-            filename = root + str(index) + suffix
-            torch.save(embeddings, filename)
-            embeddings.clear()
-            index += 1 
-            ctr = 0
 
-if embeddings:
-    filename = root + str(index) + suffix
-    torch.save(embeddings, filename)
-    embeddings.clear()
+    TOKENIZER = AutoTokenizer.from_pretrained('../mcontriever')
+    MODEL = Contriever.from_pretrained('../mcontriever').to(device)
+    MODEL.eval()
+    print("model loaded.")
+
+    index = 0
+    ctr = 0
+    root = args.output_dir + "/key_"
+    suffix = ".pt"
+    embeddings = []
+
+    with open(args.input_file, "r") as input_file:
+        for line in tqdm(input_file):
+            data = json.loads(line)
+            neighbor = data["neighbor"]
+            input = TOKENIZER(neighbor, padding=True, truncation=True, return_tensors='pt').to(device)
+            output = MODEL(**input)
+            embeddings.append(output.cpu())
+            ctr += 1
+            if ctr == args.tensors_per_file:
+                filename = root + str(index) + suffix
+                torch.save(embeddings, filename)
+                embeddings.clear()
+                index += 1 
+                ctr = 0
+
+    if embeddings:
+        filename = root + str(index) + suffix
+        torch.save(embeddings, filename)
+        embeddings.clear()
+
+if __name__ == '__main__':
+    main()
